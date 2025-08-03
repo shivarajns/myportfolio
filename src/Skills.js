68efd7fb310a2cc7c4f1d7f266114c02aeb1699e
Shivaraju NS <shivarajsmurthy@gmail.com>
2025-08-03 19:36:40 +0530

    Added animated Skills section with auto-rotating tech stack display

--- src/Skills.js
+++ src/Skills.js
@@ -1,35 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+
+const Techs = ["HTML", "CSS", "JAVASCRIPT", "JAVA"];
+
 function Skills() {
+  const [index, setIndex] = useState(0);
 
-    const Techs = ["HTML", "CSS", "JAVA SCRIPT", "JAVA"];
-    const [text, setText] = useState(Techs[0]);
-    const [index, setIndex] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex(prevIndex => (prevIndex + 1) % Techs.length);
+    }, 1500);
+    return () => clearInterval(interval);
+  }, []);
 
-    useEffect(() => {
-        const Interval = setInterval(() => {
-            setIndex(prevIndex => (prevIndex + 1) % Techs.length)
-        }, 1500)
-        return () => clearInterval(Interval)
-    }, [])
-
-    useEffect(() => {
-        setText(Techs[index])
-    }, [index])
-
-    return (
-        <>
-            <section id="Skills">
-                <div className='Skills-main-container'>
-                    <div className='Skills-main-cnt'>
-                        <div className='skills-cnt-one' style={{ color: 'black', fontSize: '24px' }}>
-                            I Know: <span id="change"> {text}.</span>
-                        </div>
-                    </div>
-
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <section id="Skills">
+      <div className="Skills-main-container">
+        <div className="Skills-main-cnt">
+          <div className="skills-cnt-one" style={{ color: 'black', fontSize: '24px' }}>
+            I Know: <span id="change">{Techs[index]}.</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Skills
+export default Skills;
