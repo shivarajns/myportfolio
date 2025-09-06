284c840851a7d1e5d54ea32aeb67cf69b0c69104
Shivaraju NS <shivarajsmurthy@gmail.com>
2025-09-06 12:36:50 +0530

    Designed Full Skill Section.

--- src/skills.js
+++ src/skills.js
@@ -3,6 +3,9 @@ import HTML from './assets/images/HTML.png'
 import java from './assets/images/java.png'
 import javascript from './assets/images/javascript.png'
 import react from './assets/images/react.png'
+import git from './assets/images/git.png'
+import github from './assets/images/github.png'
+import vscode from './assets/images/vscode.png'
 function Skills() {
 
 
@@ -33,6 +36,21 @@ function Skills() {
                     <div className='skills-card'>
                         <p>React.JS</p>
                         <img src={react} alt='React.JS'></img>
+                    </div>
+
+                    <div className='skills-card'>
+                        <p>Git</p>
+                        <img src={git} alt='Git'></img>
+                    </div>
+
+                    <div className='skills-card'>
+                        <p>Github</p>
+                        <img src={github} alt='Github'></img>
+                    </div>
+
+                    <div className='skills-card'>
+                        <p>VS Code</p>
+                        <img src={vscode} alt='VS Code'></img>
                     </div>
                 </div>
             </div>
